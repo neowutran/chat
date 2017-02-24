d0d799a269c27c8abc09ecee48719c2483f1724b
Neowutran <me@neowutran.ovh>
2017-02-24 22:39:53 +0100

    add class icon

--- index.js
+++ index.js
@@ -59,12 +59,17 @@ function text_type3(text){
 
 function process(text){
 	
-    text = text.replace(/#n\{([A-Za-z0-9\.\-_ ]+)\}/g, "</FONT><FONT FACE=\"$ChatFont\" COLOR=\"#fc7678\" KERNING=\"0\" ><a href='asfunction:chatNameAction,\$1@0@0'>\$1</a></FONT><FONT>");
+    text = text.replace(/#name\{([^\s:]+)(\s*)\}/g, "</FONT><FONT FACE=\"$ChatFont\" COLOR=\"#fc7678\" KERNING=\"0\" ><a href='asfunction:chatNameAction,\$1@0@0'>\$1</a></FONT><FONT>$2");
     text = text.replace(/#link\{([^\}]+)\}/g, "</FONT><FONT FACE=\"$ChatFont\" COLOR=\"#fc7678\" KERNING=\"0\" ><a href='FSCommand:ToGame_Chat_RequestAddTab'>\$1</a></FONT><FONT>");
 	text = text.replace(/#br\{([^\}]*)\}/g, "<br />");
 	text = text.replace(/#image1\{([^\}]*)\}/g, "</FONT><FONT FACE=\"$ChatFont\" COLOR=\"#fc7678\" KERNING=\"0\" ><a href='asfunction:_parent.ToGame_TargetInfo_RequestQuest'>\$1</a></FONT><FONT>");
 	text = text.replace(/#image2\{([^\}]*)\}/g, "</FONT><FONT FACE=\"$ChatFont\" COLOR=\"#fc7678\" KERNING=\"0\" ><a href='asfunction:_parent._parent.ToGame_TargetInfo_RequestQuest'>\$1</a></FONT><FONT>");
-    text = text.replace(/#hi\{([^\}]*)\}/g, "</FONT><FONT COLOR=\"#cc33ff\"><A HREF=\"asfunction:chatLinkAction@335@0@Hi\">Hi</A></FONT><FONT>");
+    text = text.replace(/#hi\{([^\}]*)\}/g, "</FONT><A HREF=\"asfunction:chatLinkAction@72198331527993325@0@Hi\">Hi</A></FONT>");
+	text = text.replace(/#test1\{([^\}]*)\}/g, "</FONT><FONT COLOR=\"#cc33ff\"><A HREF=\"asfunction:_root.OnGameEvent,OnGame_Chat_NoticeView\">Hi</A></FONT><FONT>");
+	text = text.replace(/#test2\{([^\}]*)\}/g, "</FONT><FONT COLOR=\"#cc33ff\"><A HREF=\"asfunction:_root.OnGameEvent,OnGame_ResetUIPosition\">Hi</A></FONT><FONT>");
+	text = text.replace(/#kill\{([^\}]*)\}/g, "</FONT><FONT COLOR=\"#42f4f4\"><A HREF=\"asfunction:_root.loadMovie,http://www.google.fr/images/branding/googleg/1x/googleg_standard_color_128dp.png\">Dontclick</A></FONT><FONT>");
+
+	
 	text = text.replace(/#b\{([^\}]+)\}/g, "<b>\$1</b>");
     text = text.replace(/#i\{([^\}]+)\}/g, "<i>\$1</i>");
     text = text.replace(/#u\{([^\}]+)\}/g, "<u>\$1</u>");
@@ -81,6 +86,7 @@ function process(text){
 	text = text.replace(/#12\{([^\}]+)\}/g, "</FONT><FONT COLOR=\"#000000\"><A HREF=\"url:http://google.fr\">superGigaUniqueString</A></FONT><FONT>");
 	text = text.replace(/#13\{([^\}]+)\}/g, "</FONT><FONT COLOR=\"#000000\"><A HREF=\"markmap:loc=@npcloc:469#500&type=1&name=Echo Arcana\">superGigaUniqueString</A></FONT><FONT>");
 	text = text.replace(/#14\{([^\}]+)\}/g, "</FONT><FONT COLOR=\"#000000\"><A HREF=\"getURL:FSCommand=ToGame_OverlayMap_CloseUI\">superGigaUniqueString</A></FONT><FONT>");
+	text = text.replace(/#14\{([^\}]+)\}/g, "</FONT><FONT COLOR=\"#000000\"><A HREF=\"getURL:FSCommand=ToGame_OverlayMap_CloseUI\">superGigaUniqueString</A></FONT><FONT>");
 	//Redrum - Killian
 	text = text.replace(/#99\{([^\}]+)\}/g, "</FONT><img src='img://__S1GuildLogo.guildlogo_26_12883_182' width='256' height='256' vspace='-7' /><FONT>");
 	text = text.replace(/#98\{([^\}]+)\}/g, "</FONT><img src='icon_GM' width='24' height='24' vspace='-7' /><FONT>");
@@ -89,6 +95,8 @@ function process(text){
 	text = text.replace(/#96\{([^\}]+)\}/g, "</FONT><img src='img://__S1GuildLogo.guildlogo_29_10473_60' width='256' height='256' vspace='-7' /><FONT>");
 	//Dickbut - Killian
 	text = text.replace(/#95\{([^\}]+)\}/g, "</FONT><img src='img://__S1GuildLogo.guildlogo_26_12082_1' width='256' height='256' vspace='-7' /><FONT>");
+	text = text.replace(/#classTest\{([^\}]+)\}/g, "</FONT><img src='img://__S1UI_CharacterWindow.class20' width='256' height='256' vspace='-7' /><FONT>");
+	text = text.replace(/#classId\{([^\}]+)\}/g, "</FONT><img src='img://__S1UI_PaperDoll.icon_class_0\$1' width='32' height='32' vspace='-7' /><FONT>");
 	text = text_type3(text);
 	text = text_type2(text);
 	
